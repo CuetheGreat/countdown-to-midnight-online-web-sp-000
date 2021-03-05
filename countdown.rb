123c474de208c5618019fbfb40e1c9ef8c518485
Clayon Stewart <c.stewart89@gmail.com>
2021-03-05 20:46:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,11 @@ def countdown(time)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  
+def countdown_with_sleep(time)
+  while time != 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+    sleep(1000)
+  end
+  return "HAPPY NEW YEAR!"
 end
